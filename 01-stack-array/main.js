6fdacee39fe57fa4651d667fbadf0e2a11ec50fb
Tara Kreft <tara.kreft@gmail.com>
2020-04-15 11:45:36 -0700

    fix typo

--- 01-stack-array/main.js
+++ 01-stack-array/main.js
@@ -9,7 +9,7 @@ export function stackPush(stack, item) {
 export function stackPop(stack) {
   if(stack.length !== 0){
     const popResult = stack[stack.length - 1];
-    stack.length = [stack.length - 1]
+    stack.length = stack.length - 1;
     return popResult;
   }
 }
