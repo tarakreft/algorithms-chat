209bd93f2dfff8bd1698ec3ee5bdeba62064160e
Tara Kreft <tara.kreft@gmail.com>
2020-05-03 23:16:38 -0700

    02 take two

--- 02-stack-list/main.js
+++ 02-stack-list/main.js
@@ -1,15 +1,37 @@
-export function stackCreate() {
-  return {};
+export function stackCreate(item) {
+  return {
+    item: item,
+    next: null
+};
+}
+
+function findNullNode(stack) {
+  let tempStack = stack;
+  if(tempStack.next && tempStack.next.hasOwnProperty("item")){
+    tempStack = tempStack.next;
+    return findNullNode(tempStack);
+  } else {
+    return tempStack;
+  }
 }
 
 export function stackPush(stack, item) {
-  const amount = Object.entries(stack).length;
-  stack[amount] = item;
+  let stackTail;
+  const newNode = stackCreate();
+  newNode.item = item;
+  stackTail = findNullNode(stack);
+  stackTail.next = newNode;
 }
 
 export function stackPop(stack) {
-  const amount = Object.entries(stack).length - 1;
-  const poppedItem = stack[amount];
-  delete stack[amount];
-  return poppedItem;
+  let stackTail;
+  let poppedItem;
+  stackTail = findNullNode(stack);
+  poppedItem = {...stackTail};
+  delete stackTail.item;
+  delete stackTail.next;
+  let newTail = findNullNode(stack);
+  newTail.next = null;
+  return poppedItem.item;
 }
+
